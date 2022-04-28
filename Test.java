cb4c104096d5001559b5fa4a8b6ee90a23b4cb02
Puru80 <puru.agar99@gmail.com>
2022-04-28 16:52:19 +0530

    Jenkins lab - commit 1

--- Test.java
+++ Test.java
@@ -1,5 +1,11 @@
 class Test{
     public static void main(String[] args) {
         System.out.println("Testing.....");
+
+        System.out.println("Course1  4");
+        System.out.println("Course2  5");
+        System.out.println("Course3  3");
+        System.out.println("Course4  1");
+        System.out.println("Course5  2");
     }
 }
